b19c7ab35486955abeb5097261026c5e1440d2d3
Diego Patrick da Silva Santos <diegosantos.datascientist@gmail.com>
2024-11-21 18:45:37 -0300

    Commit inicial do projeto

---
+++ ordem-livros/src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<div class="
+    flex flex-col justify-center 
+    items-center min-h-screen gap-5
+    bg-black text-white
+">
+	<h1 class="text-3xl font-bold">Ordem dos Livros</h1>
+    <a href="/autores">Autores</a>
+</div>
+
